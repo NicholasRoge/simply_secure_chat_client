1c3eea91ccbb1d412b677f519646b6ec032beb68
Nicholas Rogé <NicholasRoge@gmail.com>
2012-05-02 15:44:44 -0500

    -Initial commit -Created project for use with Eclipse

---
+++ src/roge/simplysecurechatclient/main/Main.java
@@ -0,0 +1,17 @@
+package roge.simplysecurechatclient.main;
+
+/**
+ * @author Nicholas Rogé
+ */
+public class Main{
+    /*Begin Main*/
+    /**
+     * Entry point for the application.
+     * 
+     * @param arguments Arguments passed to this program at runtime. 
+     */
+    public static void main(String[] args){
+        //TODO_HIGH:  Do stuff here!
+    }
+    /*End Main*/
+}
